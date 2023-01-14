79032ccc2a1ce5045b2e046d71506a26f0c84d51
b0rke-mborina <mborina@net.hr>
2023-01-14 19:19:33 +0100

    New views and classes, navbar, placeholder colors

--- src/main/java/notepadplaner/NotepadPlanerApplication.java
+++ src/main/java/notepadplaner/NotepadPlanerApplication.java
@@ -1,8 +1,10 @@
-package notepadplaner.notepadplaner;
+package notepadplaner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import notepadplaner.models.Note;
 import notepadplaner.models.TodoList;
@@ -14,17 +16,34 @@ import java.util.ArrayList;
 import java.util.SimpleTimeZone;
 
 public class NotepadPlanerApplication extends Application {
+    // private BorderPane layout;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(NotepadPlanerApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello World!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NotepadPlanerView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1440, 720);
+            stage.setTitle("Notepad planer");
+            stage.setMinWidth(720);
+            stage.setMinHeight(360);
+
+            // layout = new BorderPane();
+            // layout.setLeft(new BorderPane(new Button("OK")));
+            // setView(viewOne);
+
+            // FXMLLoader loaderNavBar = new FXMLLoader(getClass().getResource("components/NavBar.fxml"));
+            // fxmlLoader.setController(new CustomElementController()); //Or just specify the Controller in the FXML file
+            // stage.getChildren().add(fxmlLoader.load());
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        // launch();
+        launch();
 
         // Create model
         // Note myNote1 = new Note("This is title", "This is content");
@@ -61,7 +80,7 @@ public class NotepadPlanerApplication extends Application {
         */
 
         // Get TodoList example
-        TodoList myTodoList = TodoList.get(2);
+        // TodoList myTodoList = TodoList.get(2);
 
         // Edit TodoList example
         // ... to be continued
---
+++ src/main/java/notepadplaner/controllers/NotesController.java
@@ -0,0 +1,5 @@
+package notepadplaner.controllers;
+
+public class NotesController {
+
+}
