cf9240fb6e0417588663f6070fe67a3828d344ac
b0rke-mborina <mborina@net.hr>
2023-01-31 10:51:25 +0100

    FXML comments, fix

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,7 +1,8 @@
-package skip;
-
 import notepadplaner.NotepadPlanerApplication;
 
+/*
+*
+* */
 public class Launcher {
     public static void main(String[] args) {
         NotepadPlanerApplication.main(args);
